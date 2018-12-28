84cc28a510ae86116600698c9fbaa2f22d53d687
kscottwordsmith <kscottwordsmith@gmail.com>
2018-12-28 13:43:31 -0800

    finally finished minus tedious check and radio styling

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-const content = document.querySelector(".item")
+var content = document.querySelector(".item")
+
+var itemTemplate = ""
+
+function checkName(name){
+	if (name.length > 27) {
+		return name.substring(0, 23) + "..."
+	} else {
+		return name
+	}
+}
 
 items.results.forEach(function(item){
-	var itemTemplate = `
-	<a href=${item.results.url}>
-			<img src=${item.results.Images[0].url_170x135}>
-	</a>
-	<a href="${item.results.url}" id="itemTitle">${item.results.title}</a>
-	<a  id="shopName" href="${item.results.Shop.shop_name}"></a>
-	<span id="price">${item.results.price}</span>
+	itemTemplate += `
+	<div class='each'>
+		<a href="${item.url}">
+			<img src="${unescape(item.Images[0].url_170x135)}">
+		</a>
+		<a href="${item.url}" id="itemTitle">${checkName(item.title)}</a>
+		<a  id="shopName" href="#">${item.Shop.shop_name}</a>
+		<span id="price">$${item.price}</span>
+	</div>
 	`
-	content.innerHTML += itemTemplate
+	
 })
+
+content.innerHTML = itemTemplate
